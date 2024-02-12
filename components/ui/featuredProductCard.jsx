46ed45c9c1354d19c762ba4c5c8ddfe145dcdae7
khaleelibraheem <alhajikhaleel@gmail.com>
2024-02-12 17:45:05 +0530

    fix product card text

--- components/ui/featuredProductCard.jsx
+++ components/ui/featuredProductCard.jsx
@@ -10,8 +10,13 @@ export default function FeaturedProductCard({ equipment }) {
           alt={equipment.name}
           className="w-[300px] h-[310px] object-cover object-center rounded"
         />
-        <h2 className="font-bold text-lg mt-5">{equipment.name}</h2>
-        <h3 className="mt-2 text-gray-500 text-sm tracking-tight">
+        <h2 className="text-[12px] md:text-md uppercase font-semibold mt-5 text-gray-600">
+          {equipment.name}
+        </h2>
+        <h2 className="text-3xl md:text-lg font-bold tracking-tight">
+          {equipment.model}
+        </h2>
+        <h3 className="mt-1 text-gray-500 text-sm tracking-tight">
           Starting at
         </h3>
         <h3 className="mt-1 font-semibold tracking-tight text-xl">
